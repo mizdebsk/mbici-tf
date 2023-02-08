05c179a1404ea916a826dc8d36a341a2d0ca2335
Mikolaj Izdebski <mizdebsk@redhat.com>
2023-02-08 16:30:20 +0100

    Generate tmt report

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo "${PLATFORM}" | base64 -d | zcat >platform.xml
 echo "${SUBJECT}" | base64 -d | zcat >subject.xml
 /mbici-wf generate -plan plan.xml -platform platform.xml -subject subject.xml -workflow workflow.xml
 /mbici-wf run -batch -workflow workflow.xml -resultDir result -cacheDir cache -workDir work
-/mbici-wf report -plan plan.xml -platform platform.xml -subject subject.xml -workflow workflow.xml -resultDir result -reportDir "${REPORT_DIR}"
+/mbici-wf report -tmt -plan plan.xml -platform platform.xml -subject subject.xml -workflow workflow.xml -resultDir result -reportDir "${REPORT_DIR}"
