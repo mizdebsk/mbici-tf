f544330df113fbbcf25b0a0e734ade1131c23fbf
Mikolaj Izdebski <mizdebsk@redhat.com>
2023-02-08 17:47:03 +0100

    Reformat run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,25 @@ set -eux
 echo "${PLAN}" | base64 -d | zcat >plan.xml
 echo "${PLATFORM}" | base64 -d | zcat >platform.xml
 echo "${SUBJECT}" | base64 -d | zcat >subject.xml
-/mbici-wf generate -plan plan.xml -platform platform.xml -subject subject.xml -workflow workflow.xml
-/mbici-wf run -batch -workflow workflow.xml -resultDir result -cacheDir cache -workDir work
-/mbici-wf report -tmt -plan plan.xml -platform platform.xml -subject subject.xml -workflow workflow.xml -resultDir result -reportDir "${REPORT_DIR}"
+
+/mbici-wf generate \
+	  -plan plan.xml \
+	  -platform platform.xml \
+	  -subject subject.xml \
+	  -workflow workflow.xml
+
+/mbici-wf run \
+	  -batch \
+	  -workflow workflow.xml \
+	  -resultDir result \
+	  -cacheDir cache \
+	  -workDir work
+
+/mbici-wf report \
+	  -tmt \
+	  -plan plan.xml \
+	  -platform platform.xml \
+	  -subject subject.xml \
+	  -workflow workflow.xml \
+	  -resultDir result \
+	  -reportDir "${REPORT_DIR}"
