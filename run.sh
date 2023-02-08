dcce7025b071a9bb1c128cc51adff5e7fd373641
Mikolaj Izdebski <mizdebsk@redhat.com>
2023-02-08 12:21:51 +0100

    Use batch mode

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "${PLAN}" | base64 -d | zcat >plan.xml
 echo "${PLATFORM}" | base64 -d | zcat >platform.xml
 echo "${SUBJECT}" | base64 -d | zcat >subject.xml
 /mbici-wf generate -plan plan.xml -platform platform.xml -subject subject.xml -workflow workflow.xml
-/mbici-wf run -workflow workflow.xml -resultDir result -cacheDir cache -workDir work
+/mbici-wf run -batch -workflow workflow.xml -resultDir result -cacheDir cache -workDir work
